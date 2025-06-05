a9a82585b197cf466e1c86139607e20c42ac871c
Hikkun098 <mhhkn098@gmail.com>
2025-06-06 00:58:56 +0900

    feat: カテゴリ一覧API実装

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 
-# アプリケーション作成
 app = FastAPI()
 
-# 最初のAPI
 @app.get("/")
 def hello():
     return {"message": "Hello!"}
+
+# 新しく追加 ↓
+@app.get("/api/categories")
+def get_categories():
+    return {
+        "categories": [
+            {"id": "distance", "name": "Distance"},
+            {"id": "data_size", "name": "Data Size"},
+            {"id": "weight", "name": "Weight"},
+            {"id": "time", "name": "Time"},
+            {"id": "temperature", "name": "Temperature"}
+        ]
+    }
